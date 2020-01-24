cd074d9fc02e09c230b544a4a863384b7b24198f
baibai25 <kamuy.camel.0225@gmail.com>
2020-01-24 23:53:06 +0900

    Update

--- get_comment.py
+++ get_comment.py
@@ -1,9 +1,8 @@
 # Ref.
 # http://watagassy.hatenablog.com/entry/2018/10/06/002628
+# http://watagassy.hatenablog.com/entry/2018/10/08/132939
 
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import requests
 import argparse
 from itertools import repeat
@@ -11,15 +10,13 @@ from itertools import repeat
 
 class Scraping():
     
-    def __init__(self, target_url, options):
+    def __init__(self, target_url):
         self.target_url = target_url
-        self.options = options
         self.next_url = self.get_source()
 
     def get_source(self):
-        dict_str = ""
         next_url = ""
-
+        
         # get html source and live_chat_replay URL
         html = requests.get(self.target_url)
         soup = BeautifulSoup(html.text, "html.parser")
@@ -32,49 +29,50 @@ class Scraping():
 
     def get_comment(self):
         comment_data = []
-
-        for _ in repeat(None):
-            try:
-                # get next comment url
-                driver = webdriver.Chrome(options=self.options, executable_path="/usr/bin/chromedriver")
-                driver.get(self.next_url)
-                soup = BeautifulSoup(driver.page_source,"lxml")
-                driver.quit()
-
-                # find "script"
-                for scrp in soup.find_all("script"):
-                    if "window[\"ytInitialData\"]" in scrp.text:
-                        dict_str = scrp.text.split(" = ")[1]
-
-                # convert javascript format 
-                dict_str = dict_str.replace("false","False")
-                dict_str = dict_str.replace("true","True")
-
-                # convert to dictionary
-                dict_str = dict_str.rstrip("  \n;")
-                dics = eval(dict_str)
-
-                # next live_chat_replay url
-                # "https://www.youtube.com/live_chat_replay?continuation="
-                continue_url = dics["continuationContents"]["liveChatContinuation"]["continuations"][0]["liveChatReplayContinuationData"]["continuation"]
-                self.next_url = "https://www.youtube.com/live_chat_replay?continuation=" + continue_url
-                
-                # get comment data
-                for samp in dics["continuationContents"]["liveChatContinuation"]["actions"][1:]:
-                    try:
-                        comment_path = samp["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]
-                        comment_path = str(comment_path["liveChatTextMessageRenderer"]["message"]["runs"][0]["text"])+"\n"
-                        comment_data.append(comment_path)
-                    except:
-                        pass
-            except:
-                break
+        session = requests.Session()
+        headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'}
         
+        for _ in repeat(None):
+            # get next comment url
+            html = session.get(self.next_url, headers=headers)
+            soup = BeautifulSoup(html.text, "lxml")
+
+            # find "script"
+            for scrp in soup.find_all("script"):
+                if "window[\"ytInitialData\"]" in scrp.text:
+                    dict_str = scrp.text.split(" = ")[1]
+
+            # convert javascript format 
+            dict_str = dict_str.replace("false","False")
+            dict_str = dict_str.replace("true","True")
+
+            # convert to dictionary
+            dict_str = dict_str.rstrip("  \n;")
+            dics = eval(dict_str)
+
+            # next live_chat_replay url
+            # "https://www.youtube.com/live_chat_replay?continuation="
+            try:
+                continue_url = dics["continuationContents"]["liveChatContinuation"]["continuations"][0]["liveChatReplayContinuationData"]["continuation"]
+            except:
+                # not found url
+                break
+            else:
+                self.next_url = "https://www.youtube.com/live_chat_replay?continuation=" + continue_url
+            
+            # get comment data
+            for samp in dics["continuationContents"]["liveChatContinuation"]["actions"][1:]:
+                try:
+                    comment_path = samp["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]
+                    comment_path = str(comment_path["liveChatTextMessageRenderer"]["message"]["runs"][0]["text"])+"\n"
+                    comment_data.append(comment_path)
+                except:
+                    # handling super chat?
+                    pass
+
         self.save(comment_data)
 
-    
     def save(self, comment_data):
-        # save
         with open("comment_data.txt", mode='w', encoding="utf-8") as f:
             f.writelines(comment_data)
 
@@ -86,13 +84,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
     target_url = args.url
 
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-desktop-notifications')
-    options.add_argument("--disable-extensions")
-    options.add_argument('--blink-settings=imagesEnabled=false')
-
-    data = Scraping(target_url, options)
+    data = Scraping(target_url)
     data.get_comment()
     
